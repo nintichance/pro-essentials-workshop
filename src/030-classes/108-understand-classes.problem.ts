cf113ddd53bad0386cdea507dc480c8d8756e7b8
Ninti Chance <mintidance@gmail.com>
2025-07-18 17:35:40 -0400

    Add first note on Classes

--- src/030-classes/108-understand-classes.problem.ts
+++ src/030-classes/108-understand-classes.problem.ts
@@ -1,6 +1,20 @@
 import { expect, it } from "vitest";
 
-class CanvasNode {}
+// You can declare properties that are instantiated upon the constructor call in line below without
+// specifying the constructor method
+class CanvasNode {
+  readonly x: number = 0;
+  readonly y: number = 0;
+}
+
+// class CanvasNode {
+//   readonly x: number;
+//   readonly y: number;
+//   constructor(){
+//     this.x = 0;
+//     this.y = 0;
+//   }
+// }
 
 it("Should store some basic properties", () => {
   const canvasNode = new CanvasNode();
